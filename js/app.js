f0b0336546b543b6d9f3423bb9c75298bfa65bd0
Adnan <adnan@lakhaniproductions.com>
2020-09-18 16:01:32 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,12 @@
  * app.js */
 
 
-
+/**
+ *  app.js to create a new instance of the `Game` class and add event listeners for the start
+ *  button and onscreen keyboard buttons.
+ * 
+ * 
+*/
 
 
 
@@ -13,8 +18,11 @@ const button= document.getElementById('btn__reset');
 button.addEventListener('click', e => {
     const game = new Game();
     game.startGame();
+    game.handleInteraction();
+    game.checkForWin();
+    
+    game.removeLife();
 });
-
 
 
 
