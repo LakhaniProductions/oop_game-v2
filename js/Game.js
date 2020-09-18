cfa56fd6ad208e384174cab7ff04ae78c3274551
Adnan <adnan@lakhaniproductions.com>
2020-09-18 16:01:29 -0400

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -2,6 +2,10 @@
  * Project 4 - OOP Game App
  * Game.js */
 
+/* Game.js to create a Game class methods for starting and ending the game, handling
+interactions, getting a random phrase, checking for a win, and removing a life from the
+scoreboard. */
+
  class Game {
     constructor(){
         this.missed = 0;
@@ -18,7 +22,7 @@
         {phrase: 'Raining Cats and Dogs'},
         {phrase: 'You Cannot Judge a Book By Its Cover'}
     ]
-    return (phrasesArray);
+    return phrasesArray;
     }
 
     getRandomPhrase(){
@@ -31,26 +35,90 @@
     startGame(){
         const overlay= document.getElementById('overlay');
         overlay.style.display='none';
+    }
 
+    handleInteraction(){
         const randomPhrase = this.getRandomPhrase();
         const phrase = new Phrase(randomPhrase.phrase); 
         phrase.addPhraseToDisplay();
 
         this.activePhrase = randomPhrase;
+
+        //console.log(randomPhrase, this.activePhrase, phrase)
         
-        phrase.checkLetter();     
-        phrase.showMatchedLetter();                
+        //phrase.checkLetter();     
+        phrase.showMatchedLetter();     
+        
+        
+    }
+
+    checkForWin(){
+        const phraseDiv = phrase.childNodes;
+        const phraseUl = phraseDiv[1];
+        const phraseLi= phraseUl.querySelectorAll('li');
+        console.log(phraseLi.length);
+        //console.log(phraseLi.classList.contains('hide'));
+        //console.log(keysAll);
+        
+
+
+        for (let i= 0; i < phraseLi.length; i++) {
+            
+                
+
+                const phraseClass = phraseLi[i].classList;
+                const shownArray = [];
+                //console.log(phraseClass[0]!== 'hide');
+                console.log(phraseClass[0] === 'show');
+                //console.log(shownArray);
+                //console.log(phraseClass[0] !== 'hide' && phraseClass[0] !== 'space');
+                
+                //console.log(phraseLi[i].textContent,phraseClass[0], [i]);
+                //console.log(shownArray,shownArray.length);
+            
+                    // if(phraseClass[0] !== 'hide' && phraseClass[0] !== 'space'){
+                        
+                    // } else {
+                    //     shownArray.push(phraseLi);
+                    //     console.log(shownArray[0].length);
+                    // }
+            
+                
+
+
+            
+        }
+       
+      
+        
+    }
+
+    removeLife(){
+        
        
     }
 
-    handleInteraction(){
-        
-    }
-
-    checkForWin(){}
-
-    removeLife(){}
-
     gameOver(){}
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
