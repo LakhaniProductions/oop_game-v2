ea55cc37277b2497e0b022e595713277f41c952a
Adnan <adnan@lakhaniproductions.com>
2020-09-22 19:48:18 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,3 +23,43 @@ button.addEventListener('click', e => {
     game.startGame();
 });
 
+for (let i= 0; i < keysAll.length; i++) {
+    
+    keysAll[i].addEventListener('click', e => {
+        
+        let buttonLetter = e.target.textContent;
+        let buttonClicked= e.target;
+        let keyClass= buttonClicked.className;
+        //phrase.checkLetter(buttonLetter);
+        
+        if (phrase.checkLetter(buttonLetter)){
+            buttonClicked.setAttribute('class', `${keyClass} chosen`);
+            buttonClicked.style.pointerEvents='none';
+            
+            phrase.showMatchedLetter(buttonLetter);
+            game.checkForWin();
+            
+        } else {
+            buttonClicked.setAttribute('class', `${keyClass} wrong`);
+            buttonClicked.style.pointerEvents='none';
+            game.removeLife(buttonLetter);
+        }
+    }); 
+}
+
+
+document.addEventListener('keypress', (e)=>{
+    
+    let keyLetter = e.key;
+    let keyEvent= e;
+
+    game.handleInteraction(keyLEvent);
+
+    
+    game.removeLife(keyLetter);
+
+    
+    
+
+});
+
