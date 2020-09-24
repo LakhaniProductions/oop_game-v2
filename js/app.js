a910c7bda4a58dd3dcd336841d466d849d11403c
Adnan <adnan@lakhaniproductions.com>
2020-09-24 11:19:40 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,5 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
-
-
-/**
- *  app.js to create a new instance of the `Game` class and add event listeners for the start
- *  button and onscreen keyboard buttons.
- * 
- * 
-*/
-
 let game;
-
-
 const button= document.getElementById('btn__reset');
-
-
 
 button.addEventListener('click', e => {
     game = new Game();
@@ -28,7 +12,6 @@ for (let i= 0; i < keysAll.length; i++) {
         game.handleInteraction(click);
     }); 
 }
-
 
 document.addEventListener('keyup', (e)=>{
     let key= e;
