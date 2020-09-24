181e728298d608996057d501efabf3766cd6f1f8
Adnan <adnan@lakhaniproductions.com>
2020-09-24 11:19:43 -0400

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,9 +1,3 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
-// Phrase.js to create a Phrase class to handle the creation of phrases.
-
-
 let phraseDiv = document.querySelector('#phrase');
 const phraseUl = phraseDiv.childNodes[1];
 
